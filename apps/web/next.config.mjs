7a079d23b67aada68f8447e9fd22c0d86c06dcb3
Philipp Muens <philipp@muens.io>
2025-10-27 08:35:26 +0000

    perf: simplify next config

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,21 +1,5 @@
 import path from 'path'
-import withBundleAnalyzer from '@next/bundle-analyzer'
-import withPWAInit from '@ducanh2912/next-pwa'
-import remarkGfm from 'remark-gfm'
-import remarkHeadingId from 'remark-heading-id'
-import createMDX from '@next/mdx'
-import remarkFrontmatter from 'remark-frontmatter'
-import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
-import { readFile } from 'fs/promises'
-import { fileURLToPath } from 'url'
 import { execSync } from 'child_process'
-
-const SERVICE_WORKERS_PATH = './src/service-workers'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-const pkgPath = path.join(__dirname, 'package.json')
-const data = await readFile(pkgPath, 'utf-8')
-const pkg = JSON.parse(data)
 
 let commitHash = process.env.NEXT_PUBLIC_COMMIT_HASH
 if (!commitHash) {
@@ -25,35 +9,6 @@ if (!commitHash) {
     commitHash = ''
   }
 }
-
-const withPWA = withPWAInit({
-  dest: 'public',
-  workboxOptions: {
-    mode: 'production',
-  },
-  reloadOnOnline: false,
-  publicExcludes: [],
-  buildExcludes: [/./],
-  customWorkerSrc: SERVICE_WORKERS_PATH,
-  // Prefer InjectManifest for Web Push
-  swSrc: `${SERVICE_WORKERS_PATH}/index.ts`,
-
-  runtimeCaching: [
-    {
-      urlPattern: /\.(js|css|png|jpg|jpeg|gif|webp|svg|ico|ttf|woff|woff2|eot)$/,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'static-assets',
-        expiration: {
-          maxEntries: 1000,
-          maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-        },
-      },
-    },
-  ],
-
-  cacheId: pkg.version,
-})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -68,26 +23,16 @@ const nextConfig = {
     NEXT_PUBLIC_COMMIT_HASH: commitHash,
   },
 
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
   reactStrictMode: false,
   productionBrowserSourceMaps: true,
   eslint: {
-    dirs: ['src', 'cypress'],
-  },
-  experimental: {
-    optimizePackageImports: [
-      '@mui/material',
-      '@mui/icons-material',
-      'lodash',
-      'date-fns',
-      '@sentry/react',
-      '@gnosis.pm/zodiac',
-    ],
+    dirs: ['src'],
   },
   webpack(config, { dev }) {
     config.module.rules.push({
       test: /\.svg$/i,
-      issuer: { and: [/\.(js|ts|md)x?$/] },
+      issuer: { and: [/\.(js|ts)x?$/] },
       use: [
         {
           loader: '@svgr/webpack',
@@ -136,15 +81,5 @@ const nextConfig = {
     return config
   },
 }
-const withMDX = createMDX({
-  extension: /\.(md|mdx)?$/,
-  jsx: true,
-  options: {
-    remarkPlugins: [remarkFrontmatter, [remarkMdxFrontmatter, { name: 'metadata' }], remarkHeadingId, remarkGfm],
-    rehypePlugins: [],
-  },
-})
 
-export default withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-})(withPWA(withMDX(nextConfig)))
+export default nextConfig
