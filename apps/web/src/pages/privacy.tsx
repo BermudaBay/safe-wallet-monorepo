b59c7cedaafb21075eeb1a9c4789b7ff57e81f15
Philipp Muens <philipp@muens.io>
2025-10-27 08:35:00 +0000

    perf: remove markdown usage in static pages

--- apps/web/src/pages/privacy.tsx
+++ apps/web/src/pages/privacy.tsx
@@ -1,27 +1,15 @@
-import CustomLink from '@/components/common/CustomLink'
-import type { MDXComponents } from 'mdx/types'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import SafePrivacyPolicy from '@/markdown/privacy/privacy.md'
-import { useIsOfficialHost } from '@/hooks/useIsOfficialHost'
 import { BRAND_NAME } from '@/config/constants'
 
-const overrideComponents: MDXComponents = {
-  a: CustomLink,
-}
-
 const PrivacyPolicy: NextPage = () => {
-  const isOfficialHost = useIsOfficialHost()
-
   return (
     <>
       <Head>
         <title>{`${BRAND_NAME} – Privacy policy`}</title>
       </Head>
 
-      <main style={{ lineHeight: '1.5' }}>
-        {isOfficialHost && <SafePrivacyPolicy components={overrideComponents} />}
-      </main>
+      <main style={{ lineHeight: '1.5' }}>Privacy Policy</main>
     </>
   )
 }
