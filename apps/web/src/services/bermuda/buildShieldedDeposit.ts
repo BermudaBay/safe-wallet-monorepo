1c8d2899e1d7db363f373b4dae5a667727aee9ab
Yugo <yuu5infinity@gmail.com>
2025-11-04 07:01:51 +0900

    fix: clarify shield sssets CSP and pool transact encoding

--- apps/web/src/services/bermuda/buildShieldedDeposit.ts
+++ apps/web/src/services/bermuda/buildShieldedDeposit.ts
@@ -79,7 +79,14 @@ export const buildShieldedDepositMetaTxs = async ({
   })
 
   const [mappedArgs, mappedExtData] = bermudaSDK.utils.mapTransactArgs([args, extData])
-  const transactData = poolContract.interface.encodeFunctionData('transact', [mappedArgs, mappedExtData])
+
+  const TRANSACT_SIMPLE_SIGNATURE =
+    'transact((bytes,bytes32[],bytes32,bytes32[],bytes32[2],uint256,bytes32,bytes,bytes32[],bytes32,uint256,bytes32),(address,int256,address,uint256,bytes,bytes,bool,address,uint256,bytes32,address))'
+
+  const transactData = poolContract.interface.encodeFunctionData(TRANSACT_SIMPLE_SIGNATURE, [
+    mappedArgs,
+    mappedExtData,
+  ])
 
   const metaTxs: MetaTransactionData[] = []
 
