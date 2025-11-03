b1eb3ffba67d2d9c4fb13220babc8f00e077a207
Yugo <yuu5infinity@gmail.com>
2025-11-04 06:19:21 +0900

    feat: buildShieldedDeposit and ReviewShieldAssets

---
+++ apps/web/src/services/bermuda/buildShieldedDeposit.ts
@@ -0,0 +1,110 @@
+import { getBermudaSDK } from '@/hooks/bermudaSDK/useBermudaSDK'
+import { ZERO_ADDRESS } from '@safe-global/protocol-kit/dist/src/utils/constants'
+import type { MetaTransactionData } from '@safe-global/types-kit'
+import { safeParseUnits } from '@safe-global/utils/utils/formatters'
+import { sameAddress } from '@safe-global/utils/utils/addresses'
+import { Interface, ZeroAddress } from 'ethers'
+
+type BuildShieldedDepositArgs = {
+  safeAddress: string
+  shieldedAddress: string
+  tokenAddress: string
+  tokenDecimals: number
+  amount: string
+}
+
+export const buildShieldedDepositMetaTxs = async ({
+  safeAddress,
+  shieldedAddress,
+  tokenAddress,
+  tokenDecimals,
+  amount,
+}: BuildShieldedDepositArgs): Promise<{ metaTxs: MetaTransactionData[]; viewingKey?: string }> => {
+  console.info('[ShieldAssets][Builder] Preparing shielded deposit meta txs', {
+    safeAddress,
+    shieldedAddress,
+    tokenAddress,
+    tokenDecimals,
+    amount,
+  })
+
+  const bermudaSDK = getBermudaSDK()
+
+  if (!bermudaSDK) {
+    throw new Error('Bermuda SDK not initialized')
+  }
+
+  const poolContract = bermudaSDK.config.pool
+
+  if (!poolContract) {
+    throw new Error('Bermuda pool contract not configured')
+  }
+
+  const poolAddress = await poolContract.getAddress()
+  const isNativeToken = sameAddress(tokenAddress, ZERO_ADDRESS)
+  const normalizedToken = isNativeToken ? ZeroAddress : tokenAddress.toLowerCase()
+
+  const parsedAmount = safeParseUnits(amount, tokenDecimals)
+
+  if (parsedAmount === undefined) {
+    throw new Error('Invalid shielded deposit amount')
+  }
+  console.info('[ShieldAssets][Builder] Parsed deposit amount', {
+    parsedAmount: parsedAmount.toString(),
+  })
+
+  if (!shieldedAddress) {
+    throw new Error('Missing shielded address')
+  }
+
+  const shieldedKeyPair = bermudaSDK.types.KeyPair.fromAddress(shieldedAddress)
+
+  const utxo = new bermudaSDK.types.Utxo({
+    amount: parsedAmount,
+    token: normalizedToken,
+    keypair: shieldedKeyPair,
+    type: bermudaSDK.types.UtxoType.Fund,
+  })
+  console.info('[ShieldAssets][Builder] Constructed output UTXO')
+
+  const { args, extData, viewingKey } = await bermudaSDK.core.prepareTransact({
+    outputs: [utxo],
+    token: normalizedToken,
+    funder: safeAddress,
+    recipient: safeAddress,
+    fee: 0n,
+  })
+  console.info('[ShieldAssets][Builder] Prepared transact payload', {
+    hasViewingKey: Boolean(viewingKey),
+  })
+
+  const [mappedArgs, mappedExtData] = bermudaSDK.utils.mapTransactArgs([args, extData])
+  const transactData = poolContract.interface.encodeFunctionData('transact', [mappedArgs, mappedExtData])
+
+  const metaTxs: MetaTransactionData[] = []
+
+  if (!isNativeToken) {
+    const erc20Interface = new Interface(bermudaSDK.abis.ERC20_ABI)
+    const approveData = erc20Interface.encodeFunctionData('approve', [poolAddress, parsedAmount])
+
+    metaTxs.push({
+      to: tokenAddress,
+      data: approveData,
+      value: '0',
+    })
+    console.info('[ShieldAssets][Builder] Added ERC20 approval meta tx')
+  }
+
+  metaTxs.push({
+    to: poolAddress,
+    data: transactData,
+    value: isNativeToken ? parsedAmount.toString() : '0',
+  })
+
+  console.info('[ShieldAssets][Builder] Prepared final meta tx bundle', {
+    metaTxCount: metaTxs.length,
+    includesApproval: !isNativeToken,
+  })
+
+  return { metaTxs, viewingKey }
+}
