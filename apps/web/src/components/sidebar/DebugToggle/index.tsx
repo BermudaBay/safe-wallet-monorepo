20a4787268f4dd14a4a35e2551bb686fdcd37c4c
Philipp Muens <philipp@muens.io>
2025-11-10 17:55:11 -0300

    feat: remove cgw toggle

--- apps/web/src/components/sidebar/DebugToggle/index.tsx
+++ apps/web/src/components/sidebar/DebugToggle/index.tsx
@@ -1,24 +1,12 @@
-import { type ChangeEvent, type ReactElement } from 'react'
+import { type ReactElement } from 'react'
 import { Box, FormControlLabel, Switch } from '@mui/material'
-import useLocalStorage from '@/services/local-storage/useLocalStorage'
 import { setDarkMode } from '@/store/settingsSlice'
 import { useDarkMode } from '@/hooks/useDarkMode'
 import { useAppDispatch } from '@/store'
-import { LS_KEY } from '@/config/gateway'
 
 const DebugToggle = (): ReactElement => {
   const dispatch = useAppDispatch()
   const isDarkMode = useDarkMode()
-
-  const [isProdGateway = false, setIsProdGateway] = useLocalStorage<boolean>(LS_KEY)
-
-  const onToggleGateway = (event: ChangeEvent<HTMLInputElement>) => {
-    setIsProdGateway(event.target.checked)
-
-    setTimeout(() => {
-      location.reload()
-    }, 300)
-  }
 
   return (
     <Box py={2} ml={2}>
@@ -26,7 +14,6 @@ const DebugToggle = (): ReactElement => {
         control={<Switch checked={isDarkMode} onChange={(_, checked) => dispatch(setDarkMode(checked))} />}
         label="Dark mode"
       />
-      <FormControlLabel control={<Switch checked={isProdGateway} onChange={onToggleGateway} />} label="Use prod CGW" />
     </Box>
   )
 }
