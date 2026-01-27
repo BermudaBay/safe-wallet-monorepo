eb52f67e94406327c727ef4c3a336fc7ec384b86
pardis-toolabi <pardis.tlbi@gmail.com>
2026-01-27 06:20:57 +0100

    chore: clean up

--- apps/web/src/services/bermuda/dispatchShieldedTransfer.ts
+++ apps/web/src/services/bermuda/dispatchShieldedTransfer.ts
@@ -115,8 +115,6 @@ export async function dispatchShieldedTransfer(shieldedKeyPair: any, safeAddress
     ]
     console.log("𝜟𝜟𝜟𝜟𝜟 inputs", inputs.length, inputs)
     console.log("𝜟𝜟𝜟𝜟𝜟 outputs", outputs.length, outputs)
-    console.log("hereeee2")
-
     const { args, extData } = await bermudaSDK.core.prepareTransact({
         inputs,
         outputs,
@@ -126,7 +124,6 @@ export async function dispatchShieldedTransfer(shieldedKeyPair: any, safeAddress
 
     const [_args, _extData] = bermudaSDK.utils.mapTransactArgs([args, extData])
     const target = await bermudaSDK.config.pool.getAddress()
-    console.log("hereeee1")
     const data = bermudaSDK.config.pool.interface.encodeFunctionData(
         'transact((bytes,bytes32[],bytes32,bytes32[],bytes32[],uint256,bytes32,bytes,bytes32[],bytes32,uint256,bytes32),(address,int256,address,uint256,bytes[],bool,address,uint256,bytes32,address),(uint256,uint8,bytes32,bytes32))',
         [
